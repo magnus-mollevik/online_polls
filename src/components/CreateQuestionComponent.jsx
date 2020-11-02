64c84f5896cbb2fc51d458b000157e4471e926cb
GossipDolphin <mmollevik@hotmail.com>
2020-11-02 16:00:15 +0100

    created a testJson to check for how we are going to structure the json objects

---
+++ src/components/CreateQuestionComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const CreateQuestionComponent = () => {
+    return(
+        <form>
+            <p>Question</p>
+            <input type="text"></input>
+        </form>
+    )
+}
+export default CreateQuestionComponent
