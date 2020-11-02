42acb957a4bb3c75448c478d8b29129e0adca612
huseind <43807768+huseind@users.noreply.github.com>
2020-11-02 13:20:59 +0100

    laget styled NavBar

--- src/styles/styles.jsx
+++ src/styles/styles.jsx
@@ -31,4 +31,59 @@ export const FormStyle = styled.form`
 `;
 
 
+export const FormButton = styled.button`
+    margin-top: auto;
+    margin-bottom: 40px;
+    border-radius: 5px;
+    background-color: white;
+    height: 35px;
+    border: 0px solid transparent;
+
+    :hover{
+        background-color: darkgray;
+        color: white;
+        cursor: pointer;
+    }
+`;
+
+export const NavBar = styled.nav`
+    display: flex;
+    background-color: #000000;
+    opacity: 0.72;
+
+    ul{
+        display: flex;
+        list-style: none;
+        color: white;
+        text-decoration: none;
+        flex-direction: row-reverse;
+        height: 50px;
+        flex-grow: inherit;
+        margin: 0;
+        margin-left: auto;
+    }
+
+    li{
+        margin: auto 20px;
+    }
+
+    p{
+        float: left;
+        color: white;
+        margin-left: 10px;
+    }
+
+    ul>li>a{
+        text-decoration: none;
+        color: white;
+        text-align: center;
+    }
+
+    ul>li>a:hover{
+        cursor: pointer;
+         text-decoration: underline;
+    }
+`;
+
+
 
